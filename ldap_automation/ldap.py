0d0000001692d4ee9e46dde195a8395ee4ae200e
Amin Salamov <a.salamov@adra.gov.az>
2023-06-01 14:36:43 +0400

    refactoring

--- ldap_automation/ldap.py
+++ ldap_automation/ldap.py
@@ -1,46 +1,43 @@
-import requests
 import ldap
+from guacamole import Guacamole
 
-# Setup LDAP connection
-conn = ldap.initialize('ldap://your_ad_server')
-conn.simple_bind_s('user_dn', 'password')
+# connect to LDAP and Guacamole API
+ldap_connection = ldap.initialize(LDAP_SERVER)
+ldap_connection.simple_bind_s(USERNAME, PASSWORD)
+guacamole = Guacamole(GUACAMOLE_SERVER)
 
-# Get AD users and groups
-ad_users = conn.search_s('ou=users,dc=example,dc=com', ldap.SCOPE_SUBTREE, 'objectclass=user')
+# mappings of groups to connections
+group_to_connections = {
+    "guacamole_users": ["rdp1", "rdp2"],
+    # add more groups and connections as needed
+}
 
-# Setup Guacamole API session
-s = requests.Session()
-s.post('http://guacamole-server/api/tokens', json={'username': 'admin', 'password': 'admin'})
+# get all users in 'guacamole_users' group
+group_dn = "CN=guacamole_users,OU=groups,OU=sentrium,DC=ad,DC=sentrium,DC=io"
+guacamole_users = ldap_connection.search_s(
+    group_dn, ldap.SCOPE_SUBTREE, "(objectClass=*)", ["member"]
+)
 
-# Get Guacamole users
-guacamole_users = s.get('http://guacamole-server/api/session/data/ldap/users').json()
+# loop through each user
+for user_dn in guacamole_users[0][1]["member"]:
+    # check if user already exists in Guacamole
+    if not guacamole.user_exists(user_dn):
+        # create user in Guacamole
+        guacamole.create_user(user_dn)
 
-# Iterate over AD users
-for user in ad_users:
-    # Fetch the corresponding user in Guacamole
-    guacamole_user = next((u for u in guacamole_users if u['username'] == user[1]['uid'][0].decode()), None)
-    
-    # If user exists in Guacamole, update their group membership
-    if guacamole_user:
-        # Fetch AD groups for user
-        ad_groups = [group.decode().split(',')[0][3:] for group in user[1].get('memberOf', [])]
-        
-        # Fetch Guacamole groups for user
-        guacamole_groups = s.get(f'http://guacamole-server/api/session/data/ldap/users/{guacamole_user["username"]}/groups').json()
-        
-        # Find any AD groups the user is a member of that they're not a member of in Guacamole
-        groups_to_add = [group for group in ad_groups if group not in guacamole_groups]
-        
-        # Add user to those groups in Guacamole
-        for group in groups_to_add:
-            s.put(f'http://guacamole-server/api/session/data/ldap/users/{guacamole_user["username"]}/groups/{group}')
-            
-        # Find any Guacamole groups the user is a member of that they're not a member of in AD
-        groups_to_remove = [group for group in guacamole_groups if group not in ad_groups]
-        
-        # Remove user from those groups in Guacamole
-        for group in groups_to_remove:
-            s.delete(f'http://guacamole-server/api/session/data/ldap/users/{guacamole_user["username"]}/groups/{group}')
+    # get all groups for this user
+    user_groups = ldap_connection.search_s(
+        user_dn, ldap.SCOPE_SUBTREE, "(objectClass=*)", ["memberOf"]
+    )
 
-conn.unbind_s()
+    # loop through each group
+    for group_dn in user_groups[0][1]["memberOf"]:
+        # check if group is in our mappings
+        if group_dn in group_to_connections:
+            # get connections for this group
+            connections = group_to_connections[group_dn]
 
+            # loop through each connection
+            for connection in connections:
+                # create connection for this user
+                guacamole.create_connection(user_dn, connection)
