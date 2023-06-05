48160f435ff3a9828f8c608c475d35de59226cca
Amin Salamov <a.salamov@adra.gov.az>
2023-06-05 09:35:41 +0400

    refactoring

--- ldap_automation/ldap.py
+++ ldap_automation/ldap.py
@@ -1,43 +1,88 @@
-import ldap
-from guacamole import Guacamole
+import ldap3
+import requests
 
-# connect to LDAP and Guacamole API
-ldap_connection = ldap.initialize(LDAP_SERVER)
-ldap_connection.simple_bind_s(USERNAME, PASSWORD)
-guacamole = Guacamole(GUACAMOLE_SERVER)
+# Your LDAP and Guacamole server details
+LDAP_SERVER = "ad.sentrium.io"
+LDAP_PORT = 389
+LDAP_USER = "guacamole"
+LDAP_PASS = "=VR'2~WHs)vG#l@bbr,h"
 
-# mappings of groups to connections
+# Guacamole server details
+GUACAMOLE_SERVER = "https://guacamole.sentrium.net"
+GUACAMOLE_USER = "sentrium"
+GUACAMOLE_PASS = "pR-zv;FO)~m%fDr2kt]J"
+
+# Connect to LDAP and Guacamole API
+server = ldap3.Server(LDAP_SERVER, port=LDAP_PORT, get_info=ldap3.ALL)
+conn = ldap3.Connection(server, LDAP_USER, LDAP_PASS, auto_bind=True)
+
+# Bind to the LDAP server
+if not conn.bind():
+    print("Error in bind", conn.result)
+
+# Mappings of groups to connections
 group_to_connections = {
     "guacamole_users": ["rdp1", "rdp2"],
-    # add more groups and connections as needed
+    # Add more groups and connections as needed
 }
 
-# get all users in 'guacamole_users' group
+# Get all users in 'guacamole_users' group
 group_dn = "CN=guacamole_users,OU=groups,OU=sentrium,DC=ad,DC=sentrium,DC=io"
-guacamole_users = ldap_connection.search_s(
-    group_dn, ldap.SCOPE_SUBTREE, "(objectClass=*)", ["member"]
-)
+if not conn.search(
+    search_base=group_dn, search_filter="(objectClass=*)", attributes=["member"]
+):
+    print("Error in search", conn.result)
+guacamole_users = conn.entries
 
-# loop through each user
-for user_dn in guacamole_users[0][1]["member"]:
-    # check if user already exists in Guacamole
-    if not guacamole.user_exists(user_dn):
-        # create user in Guacamole
-        guacamole.create_user(user_dn)
+# Loop through each user
+for user_entry in guacamole_users:
+    user_dn = user_entry.entry_dn
 
-    # get all groups for this user
-    user_groups = ldap_connection.search_s(
-        user_dn, ldap.SCOPE_SUBTREE, "(objectClass=*)", ["memberOf"]
+    # Get user details from Guacamole API
+    response = requests.get(
+        f"{GUACAMOLE_SERVER}/api/session/data/mysql/users/{user_dn}",
+        auth=(GUACAMOLE_USER, GUACAMOLE_PASS),
     )
+    response.raise_for_status()
 
-    # loop through each group
-    for group_dn in user_groups[0][1]["memberOf"]:
-        # check if group is in our mappings
-        if group_dn in group_to_connections:
-            # get connections for this group
-            connections = group_to_connections[group_dn]
+    # If user doesn't exist, create it
+    if response.status_code != 200:
+        user_data = {
+            "username": user_dn,
+            "password": "UserPassword",
+        }  # Add other necessary user attributes here
+        response = requests.post(
+            f"{GUACAMOLE_SERVER}/api/session/data/mysql/users",
+            data=user_data,
+            auth=(GUACAMOLE_USER, GUACAMOLE_PASS),
+        )
+        response.raise_for_status()
 
-            # loop through each connection
+    # Get all groups for this user
+    if not conn.search(
+        search_base=user_dn, search_filter="(objectClass=*)", attributes=["memberOf"]
+    ):
+        print("Error in search", conn.result)
+    user_groups = conn.entries
+
+    # Loop through each group
+    for group_entry in user_groups:
+        group_dn = group_entry.entry_dn
+
+        # Check if group is in our mappings
+        cn = ldap3.utils.dn.parse_dn(group_dn)[0][1]
+        if cn in group_to_connections:
+            # Get connections for this group
+            connections = group_to_connections[cn]
+            # Loop through each connection
             for connection in connections:
-                # create connection for this user
-                guacamole.create_connection(user_dn, connection)
+                # Create connection for this user
+                connection_data = {
+                    "identifier": connection
+                }  # Add other necessary connection attributes here
+                response = requests.post(
+                    f"{GUACAMOLE_SERVER}/api/session/data/mysql/users/{user_dn}/connections",
+                    data=connection_data,
+                    auth=(GUACAMOLE_USER, GUACAMOLE_PASS),
+                )
+                response.raise_for_status()
